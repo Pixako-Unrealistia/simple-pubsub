73a1c08ceab834cb739ef2d253da302fa3149a03
Pixako <62527607+Pixako-Unrealistia@users.noreply.github.com>
2025-01-03 21:34:40 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,116 +1,115 @@
 // interfaces
 interface IEvent {
-  type(): string;
-  machineId(): string;
+	type(): string;
+	machineId(): string;
 }
 
 interface ISubscriber {
-  handle(event: IEvent): void;
+	handle(event: IEvent): void;
 }
 
 interface IPublishSubscribeService {
-  publish (event: IEvent): void;
-  subscribe (type: string, handler: ISubscriber): void;
-  // unsubscribe ( /* Question 2 - build this feature */ );
+	publish (event: IEvent): void;
+	subscribe (type: string, handler: ISubscriber): void;
+	// unsubscribe ( /* Question 2 - build this feature */ );
 }
 
 
 // implementations
 class MachineSaleEvent implements IEvent {
-  constructor(private readonly _sold: number, private readonly _machineId: string) {}
+	constructor(private readonly _sold: number, private readonly _machineId: string) {}
 
-  machineId(): string {
-    return this._machineId;
-  }
+	machineId(): string {
+		return this._machineId;
+	}
 
-  getSoldQuantity(): number {
-    return this._sold
-  }
+	getSoldQuantity(): number {
+		return this._sold
+	}
 
-  type(): string {
-    return 'sale';
-  }
+	type(): string {
+		return 'sale';
+	}
 }
 
 class MachineRefillEvent implements IEvent {
-  constructor(private readonly _refill: number, private readonly _machineId: string) {}
+	constructor(private readonly _refill: number, private readonly _machineId: string) {}
 
-  machineId(): string {
-    throw new Error("Method not implemented.");
-  }
+	machineId(): string {
+		throw new Error("Method not implemented.");
+	}
 
-  type(): string {
-    throw new Error("Method not implemented.");
-  }
+	type(): string {
+		throw new Error("Method not implemented.");
+	}
 }
 
 class MachineSaleSubscriber implements ISubscriber {
-  public machines: Machine[];
+	public machines: Machine[];
 
-  constructor (machines: Machine[]) {
-    this.machines = machines; 
-  }
+	constructor (machines: Machine[]) {
+		this.machines = machines; 
+	}
 
-  handle(event: MachineSaleEvent): void {
-    this.machines[2].stockLevel -= event.getSoldQuantity();
-  }
+	handle(event: MachineSaleEvent): void {
+		this.machines[2].stockLevel -= event.getSoldQuantity();
+	}
 }
 
 class MachineRefillSubscriber implements ISubscriber {
-  handle(event: IEvent): void {
-    throw new Error("Method not implemented.");
-  }
+	handle(event: IEvent): void {
+		throw new Error("Method not implemented.");
+	}
 }
 
 
 // objects
 class Machine {
-  public stockLevel = 10;
-  public id: string;
+	public stockLevel = 10;
+	public id: string;
 
-  constructor (id: string) {
-    this.id = id;
-  }
+	constructor (id: string) {
+		this.id = id;
+	}
 }
 
 
 // helpers
 const randomMachine = (): string => {
-  const random = Math.random() * 3;
-  if (random < 1) {
-    return '001';
-  } else if (random < 2) {
-    return '002';
-  }
-  return '003';
-
+	const random = Math.random() * 3;
+	if (random < 1) {
+		return '001';
+	} else if (random < 2) {
+		return '002';
+	}
+	return '003';
 }
 
 const eventGenerator = (): IEvent => {
-  const random = Math.random();
-  if (random < 0.5) {
-    const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
-    return new MachineSaleEvent(saleQty, randomMachine());
-  } 
-  const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
-  return new MachineRefillEvent(refillQty, randomMachine());
+	const random = Math.random();
+	if (random < 0.5) {
+		const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
+		return new MachineSaleEvent(saleQty, randomMachine());
+	} 
+	const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
+	return new MachineRefillEvent(refillQty, randomMachine());
 }
 
 
 // program
 (async () => {
-  // create 3 machines with a quantity of 10 stock
-  const machines: Machine[] = [ new Machine('001'), new Machine('002'), new Machine('003') ];
+	// create 3 machines with a quantity of 10 stock
+	const machines: Machine[] = [ new Machine('001'), new Machine('002'), new Machine('003') ];
 
-  // create a machine sale event subscriber. inject the machines (all subscribers should do this)
-  const saleSubscriber = new MachineSaleSubscriber(machines);
+	// create a machine sale event subscriber. inject the machines (all subscribers should do this)
+	const saleSubscriber = new MachineSaleSubscriber(machines);
 
-  // create the PubSub service
-  const pubSubService: IPublishSubscribeService = null as unknown as IPublishSubscribeService; // implement and fix this
+	// create the PubSub service
+	const pubSubService: IPublishSubscribeService = null as unknown as IPublishSubscribeService; // implement and fix this
 
-  // create 5 random events
-  const events = [1,2,3,4,5].map(i => eventGenerator());
+	// create 5 random events
+	const events = [1,2,3,4,5].map(i => eventGenerator());
 
-  // publish the events
-  events.map(pubSubService.publish);
+	// publish the events
+	events.map(pubSubService.publish);
 })();
